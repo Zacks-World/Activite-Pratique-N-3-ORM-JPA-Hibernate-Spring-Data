e21c52cbd43087371575c0e707492fe77a5ab44f
Zacks_World <elguazzarzakaria10@gmail.com>
2025-03-11 16:40:09 +0000

    Project Files

---
+++ src/main/java/org/example/roleuser/RoleUserApplication.java
@@ -0,0 +1,13 @@
+package org.example.roleuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoleUserApplication.class, args);
+    }
+
+}
