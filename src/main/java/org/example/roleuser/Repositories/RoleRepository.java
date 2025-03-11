fd5235c775e98f2c377daf9f489c1d3eacdf9a1e
Zacks_World <elguazzarzakaria10@gmail.com>
2025-03-11 17:23:43 +0000

    Adding the findByRoleName method to RoleRepository

---
+++ src/main/java/org/example/roleuser/Repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package org.example.roleuser.Repositories;
+
+import org.example.roleuser.Entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRoleName(String roleName);
+}
