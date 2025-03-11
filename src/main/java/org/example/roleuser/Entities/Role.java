a4be76cd5cd0d68f8002d89e0182c0a3eae7935e
Zacks_World <elguazzarzakaria10@gmail.com>
2025-03-11 21:25:09 +0000

    Hide the password and users from the json file

--- src/main/java/org/example/roleuser/Entities/Role.java
+++ src/main/java/org/example/roleuser/Entities/Role.java
@@ -1,5 +1,6 @@
 package org.example.roleuser.Entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,5 +28,6 @@ public class Role {
 //            inverseJoinColumns = @JoinColumn(name = "user_id")
 //    )
     @ToString.Exclude
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<User> users = new ArrayList<>();
 }
--- src/main/java/org/example/roleuser/Entities/User.java
+++ src/main/java/org/example/roleuser/Entities/User.java
@@ -1,5 +1,6 @@
 package org.example.roleuser.Entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,6 +21,7 @@ public class User {
     private String Userid;
     @Column(unique = true,length = 20)
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     @ManyToMany(mappedBy = "users", fetch = FetchType.EAGER)
     private List<Role> roles = new ArrayList<>();
