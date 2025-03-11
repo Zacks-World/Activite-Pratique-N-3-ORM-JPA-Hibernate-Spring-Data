e2ef3ebfc9c0198e59cfb0edb0c43fde28362da6
Zacks_World <elguazzarzakaria10@gmail.com>
2025-03-11 18:06:56 +0000

    Some tests

--- src/main/java/org/example/roleuser/RoleUserApplication.java
+++ src/main/java/org/example/roleuser/RoleUserApplication.java
@@ -1,13 +1,50 @@
 package org.example.roleuser;
 
+import org.example.roleuser.Entities.Role;
+import org.example.roleuser.Entities.User;
+import org.example.roleuser.Service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class RoleUserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RoleUserApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(UserService userService){
+        return args ->{
+            System.out.println("**********************************");
+            User user = new User();
+            user.setUsername("user1");
+            user.setPassword("123456");
+            User user_b=userService.addNewUser(user);
+            System.out.println(user_b.toString());
+            System.out.println("**********************************");
+            User user1 = new User();
+            user1.setUsername("admin");
+            user1.setPassword("123456");
+            User user_b1=userService.addNewUser(user1);
+            System.out.println(user_b1.toString());
+            System.out.println("**********************************");
+            Stream.of("ADMIN", "STUDENT", "USER").forEach(roleName -> {
+                Role role = new Role();
+                role.setRoleName(roleName);
+                Role role_b=userService.addNewRole(role);
+                System.out.println(role_b.toString());
+                System.out.println("**********************************");
+            });
+            userService.addRoleToUser("user1", "STUDENT");
+            userService.addRoleToUser("user1", "USER");
+            userService.addRoleToUser("admin", "ADMIN");
+            userService.addRoleToUser("admin", "USER");
+        };
     }
 
 }
