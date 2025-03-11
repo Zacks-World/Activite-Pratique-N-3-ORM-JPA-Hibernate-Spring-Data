5d3623e115492d60fd7fb451f78e0d8d521aba32
Zacks_World <elguazzarzakaria10@gmail.com>
2025-03-11 17:21:09 +0000

    Creation of UserService

---
+++ src/main/java/org/example/roleuser/Service/UserService.java
@@ -0,0 +1,13 @@
+package org.example.roleuser.Service;
+
+import org.example.roleuser.Entities.Role;
+import org.example.roleuser.Entities.User;
+
+public interface UserService {
+    User addNewUser(User user);
+    Role addNewRole(Role role);
+    User findUserByUserName(String userName);
+    Role findRoleByRoleName(String roleName);
+    void addRoleToUser(String username,String roleName);
+
+}
