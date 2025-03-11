064f3e6713e9f82eea91b95f64a8a0d8888dd36d
Zacks_World <elguazzarzakaria10@gmail.com>
2025-03-11 18:05:31 +0000

    Adding @ToString into Role.java and User.java

--- src/main/java/org/example/roleuser/Entities/User.java
+++ src/main/java/org/example/roleuser/Entities/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +13,11 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class User {
     @Id
     private String Userid;
-    @Column(unique = true, nullable = false,length = 20, name = "username")
+    @Column(unique = true, nullable = false,length = 20)
     private String username;
     private String password;
     @ManyToMany(mappedBy = "users", fetch = FetchType.EAGER)
